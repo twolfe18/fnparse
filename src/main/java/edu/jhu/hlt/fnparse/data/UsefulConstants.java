728d4a06b42c63699b030de22b221f334afb425a
Travis Wolfe <twolfe18@gmail.com>
2014-01-16 09:59:25 -0500

    updated some stuff to use File instead of String

--- src/main/java/edu/jhu/hlt/fnparse/data/UsefulConstants.java
+++ src/main/java/edu/jhu/hlt/fnparse/data/UsefulConstants.java
@@ -1,11 +1,14 @@
 package edu.jhu.hlt.fnparse.data;
 
+import java.io.File;
+
 public class UsefulConstants {
-    public static final int framesInFrameNet = 1019 ; // The number of frames in Framenet 1.5
-    public static final String dataPath =  System.getProperty("toydataPath");
-    public static final String frameIndexXMLPath = dataPath + "//frameIndex.xml";
-    public static final String frameXMLDirPath = dataPath + "//frame//";
-    public static final String fullTextXMLDirPath = dataPath + "//fulltext//";
+	
+	public static final int framesInFrameNet = 1019 ; // The number of frames in Framenet 1.5
+	public static final File dataPath =  new File("toydata");	//System.getProperty("toydataPath");
+	public static final File frameIndexXMLPath = new File(dataPath, "frameIndex.xml");
+	public static final File frameXMLDirPath = new File(dataPath, "frame/");
+	public static final File fullTextXMLDirPath = new File(dataPath, "fulltext/");
 }
     
     
--- src/main/java/edu/jhu/hlt/fnparse/util/Frame.java
+++ src/main/java/edu/jhu/hlt/fnparse/util/Frame.java
@@ -8,51 +8,61 @@ import edu.jhu.hlt.fnparse.data.UsefulConstants;
 import edu.jhu.hlt.fnparse.data.DataUtil;
 
 public class Frame {
-	
-    private int idx;
-    private String name;
-    private String[] lexicalUnit;
-    private String[] role;
-	
-    public Frame(int id, String name, String[] lexicalUnit, String[] role) {
-	this.idx = id;
-	this.name = name;
-	this.lexicalUnit = lexicalUnit;
-	this.role = role;
-    }
-    
-    public int getId() { return idx; }
-    public String getLexicalUnit(int i) { return lexicalUnit[i]; }
-    public int numLexicalUnits() { return lexicalUnit.length; }
-    public String getRow(int i) { return role[i]; }
-    public int numRoles() { return role.length; }
-    public String getName() { return name; }
-    
-    /**
-     * Frame used to indicate that a word does not evoke a frame
-     */
-    public static final Frame NULL_FRAME = new Frame(0, "NOT-A-FRAME", null, null);
-    
-    /**
-     * includes NULL_FRAME
-     */
-    public static List<Frame> allFrames() {
-	List<Frame> frameList = new Vector<Frame>();
-	try{
-	    HashMap tmp = DataUtil.parseFrameIndexXML(UsefulConstants.frameIndexXMLPath, UsefulConstants.framesInFrameNet);
-	    int id[] = (int[]) tmp.get("id");
-	    String name[] = (String []) tmp.get("name");	    
-	    for(int i=0; i < UsefulConstants.framesInFrameNet; i++){
-		HashMap tmp2 = DataUtil.lexicalUnitAndRolesOfFrame(name[i]);
-		String lexicalUnit[] = (String []) tmp2.get("lexicalUnit");
-		String role[] = (String []) tmp2.get("role");	    
-		frameList.add(new Frame(id[i], name[i], lexicalUnit, role));
-	    }
-	    frameList.add(NULL_FRAME);
+
+	private int idx;
+	private String name;
+	private String[] lexicalUnit;
+	private String[] role;
+
+	public Frame(int id, String name, String[] lexicalUnit, String[] role) {
+		this.idx = id;
+		this.name = name;
+		this.lexicalUnit = lexicalUnit;
+		this.role = role;
 	}
-	catch(Exception e){
-	     e.printStackTrace();System.exit(-1);
-	    }
-	return frameList;
-    }    
+	
+	public String toString() {
+		return String.format("<Frame %d %s>", idx, name);
+	}
+
+	public int getId() { return idx; }
+	public String getLexicalUnit(int i) { return lexicalUnit[i]; }
+	public int numLexicalUnits() { return lexicalUnit.length; }
+	public String getRow(int i) { return role[i]; }
+	public int numRoles() { return role.length; }
+	public String getName() { return name; }
+
+	/**
+	 * Frame used to indicate that a word does not evoke a frame
+	 */
+	public static final Frame NULL_FRAME = new Frame(0, "NOT-A-FRAME", null, null);
+
+	/**
+	 * includes NULL_FRAME
+	 */
+	public static List<Frame> allFrames() {
+		List<Frame> frameList = new Vector<Frame>();
+		try{
+			HashMap tmp = DataUtil.parseFrameIndexXML(UsefulConstants.frameIndexXMLPath, UsefulConstants.framesInFrameNet);
+			int id[] = (int[]) tmp.get("id");
+			String name[] = (String []) tmp.get("name");	    
+			for(int i=0; i < UsefulConstants.framesInFrameNet; i++){
+				HashMap tmp2 = DataUtil.lexicalUnitAndRolesOfFrame(name[i]);
+				String lexicalUnit[] = (String []) tmp2.get("lexicalUnit");
+				String role[] = (String []) tmp2.get("role");	    
+				frameList.add(new Frame(id[i], name[i], lexicalUnit, role));
+			}
+			frameList.add(NULL_FRAME);
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+		return frameList;
+	}
+	
+	public static void main(String[] args) {
+		for(Frame f : allFrames()) {
+			System.out.println(f);
+		}
+	}
 }
