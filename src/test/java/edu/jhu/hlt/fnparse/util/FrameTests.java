78e5c528d9566d1df39d35168bff5c575db87405
Travis Wolfe <twolfe18@gmail.com>
2014-01-16 11:22:56 -0500

    made Frame test more strict

--- src/test/java/edu/jhu/hlt/fnparse/util/FrameTests.java
+++ src/test/java/edu/jhu/hlt/fnparse/util/FrameTests.java
@@ -10,12 +10,18 @@ public class FrameTests {
 		List<Frame> allFrames = Frame.allFrames();
 		long time = System.currentTimeMillis() - start;
 		Set<Integer> ids = new HashSet<Integer>();
+		int max = -1;
 		for(Frame f : allFrames) {
 			System.out.println(f);
 			if(!ids.add(f.getId()))
 				throw new RuntimeException("f=" + f);
+			if(f.getId() > max)
+				max = f.getId();
 		}
 		System.out.printf("reading %d frames took %.2f sec\n", allFrames.size(), time/1000d);
+		if(max != allFrames.size()-1)
+			throw new RuntimeException("max=" + max + ", allFrames.size=" + allFrames.size());
+		
 	}
 	
 }
