aadbafdc7cde572b90509576c1c6bd79f6783ca8
Travis Wolfe <twolfe18@gmail.com>
2014-01-29 18:12:27 -0500

    Span now interns its instances

--- src/main/java/edu/jhu/hlt/fnparse/datatypes/Span.java
+++ src/main/java/edu/jhu/hlt/fnparse/datatypes/Span.java
@@ -5,11 +5,45 @@ public class Span {
 	public int start;	// inclusive
 	public int end;		// non-inclusive
 
+	public static final Span nullSpan = new Span();
+	
+	// intern instances of Span just like String
+	// also, you can use ==
+	private static int internedMaxSentSize = 30;
+	private static Span[][] internedSpans = new Span[0][0];
+	
+	public static Span getSpan(int start, int end) {
+		
+		// make a bigger table if the previous was too small
+		if(start >= internedSpans.length) {
+			internedMaxSentSize = end + 10;
+			
+			// sanity check
+			if(internedMaxSentSize > 100)
+				throw new IllegalStateException("what are you doing with these huge sentences?");
+			
+			Span[][] newInternedSpans = new Span[internedMaxSentSize-1][];
+			for(int s=0; s<internedMaxSentSize; s++) {
+				newInternedSpans[s] = new Span[internedMaxSentSize - s - 1];
+				for(int e=s+1; e<internedMaxSentSize; e++) {
+					int ei = e - s - 1;
+					// use old value if possible (needed to ensure == works)
+					newInternedSpans[s][ei] =
+							s < internedSpans.length && ei < internedSpans[s].length
+								? internedSpans[s][ei]
+								: new Span(s, e);
+				}
+			}
+			internedSpans = newInternedSpans;
+		}
+		return internedSpans[start][end - start - 1];
+	}
+	
 	/**
 	 * @param start is inclusive, 0-indexed
 	 * @param end is non-inclusive, 0-indexed
 	 */
-	public Span(int start, int end) {
+	private Span(int start, int end) {
 		assert end > start;
 		assert start >= 0;
 		this.start = start;
@@ -60,10 +94,8 @@ public class Span {
 		return false;
 	}
 	
-	public static final Span nullSpan = new Span();
-	
 	public static Span widthOne(int wordIdx) {
-		return new Span(wordIdx, wordIdx+1);
+		return getSpan(wordIdx, wordIdx+1);
 	}
 }
 
