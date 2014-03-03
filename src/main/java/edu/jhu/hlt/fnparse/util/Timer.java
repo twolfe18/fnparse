a7ded7de30fea21d6e0160eb0f46a3bee50574ac
Travis Wolfe <twolfe18@gmail.com>
2014-03-02 21:50:19 -0500

    moved Timer out to its own class

---
+++ src/main/java/edu/jhu/hlt/fnparse/util/Timer.java
@@ -0,0 +1,42 @@
+package edu.jhu.hlt.fnparse.util;
+
+public class Timer {
+	private String id;
+	private int count;
+	private long time;
+	private long lastStart = -1;
+	private int printIterval;
+	
+	public Timer(String id) {
+		this.id = id;
+		printIterval = -1;
+	}
+	public Timer(String id, int printInterval) {
+		this.id = id;
+		this.printIterval = printInterval;
+	}
+	
+	public void start() {
+		lastStart = System.currentTimeMillis();
+	}
+	
+	public long stop() {
+		long t = System.currentTimeMillis() - lastStart;
+		time += t;
+		count++;
+		if(printIterval > 0 && count % printIterval == 0)
+			System.out.printf("<Timer %s %.2f sec total, %.1f k call/sec\n", id, time/1000d, ((double)count)/time);
+		return t;
+	}
+	
+	public double totalTimeInSec() { return time / 1000d; }
+	
+	public static final class NoOp extends Timer {
+		public NoOp(String id) { super(id); }
+		public NoOp(String id, int printInterval)  { super(id, printInterval); }
+		public void start() {}
+		public long stop() { return -1; }
+	}
+	
+	public static final Timer noOp = new NoOp("noOp");
+}
