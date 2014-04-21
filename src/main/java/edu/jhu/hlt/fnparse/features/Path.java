9643644063c695e17bd024995962d9fb1780bb0d
Travis Wolfe <twolfe18@gmail.com>
2014-04-21 17:56:18 -0400

    fixed up Path features in BasicFrameFeatures and Path

--- src/main/java/edu/jhu/hlt/fnparse/features/Path.java
+++ src/main/java/edu/jhu/hlt/fnparse/features/Path.java
@@ -2,6 +2,7 @@ package edu.jhu.hlt.fnparse.features;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,33 +10,56 @@ import edu.jhu.hlt.fnparse.datatypes.Sentence;
 
 public class Path {
 	
-	enum RuleType {
+	enum NodeType {
 		CFG,		// requires a constituency parse, not yet supported
-		
-		// the following use dependency parses
-		DEP,		// assumes labeled dependency parse
 		LEMMA,
 		POS,
 		NONE		// puts in a "*" for every head/phrase
 	}
 	
+	enum EdgeType {
+		DIRECTION,		// either "<" for up or ">" for down
+		DEP				// typed dependency label
+	}
+	
 	private final int start, end;
 	private final Sentence sent;
 	private final int n;
-	private final RuleType ruleType;
+	private final NodeType nodeType;
+	private final EdgeType edgeType;
 
 	private String top;
-	private List<String> up, down;
+	private List<String> upNodes, downNodes;
+	private List<String> upEdges, downEdges;
 	private boolean connected;
+	private boolean toRoot;		// if true, end and down don't mean anything
+	
+	/** path from root to head */
+	public Path(Sentence s, int head, NodeType nodeType, EdgeType edgeType) {
+		this(s, head, -1, nodeType, edgeType, true);
+	}
 
-	public Path(Sentence s, int start, int end, RuleType ruleType) {
+	/** path between two tokens */
+	public Path(Sentence s, int start, int end, NodeType nodeType, EdgeType edgeType) {
+		this(s, start, end, nodeType, edgeType, false);
+	}
+
+	private Path(Sentence s, int start, int end, NodeType nodeType, EdgeType edgeType, boolean toRoot) {
+		
+		if(start < 0)
+			throw new IllegalArgumentException();
+		if(!toRoot && end < 0)
+			throw new IllegalArgumentException();
+		
 		this.sent = s;
 		this.start = start;
 		this.end = end;
 		this.n = sent.size();
-		this.ruleType = ruleType;
+		this.nodeType = nodeType;
+		this.edgeType = edgeType;
+		this.toRoot = toRoot;
 		
-		if(ruleType == RuleType.CFG)
+		if(nodeType == NodeType.CFG)
 			throw new RuntimeException("not supported yet");
 	
 		// the path from start to root, counting up from 0
@@ -43,66 +67,128 @@ public class Path {
 		Arrays.fill(upIndices, -1);
 		
 		// start from start and work your way up to root
-		up = new ArrayList<String>();
+		upNodes = new ArrayList<String>();
+		upEdges = new ArrayList<String>();
 		int ptr = start;
 		while(ptr >= 0 && ptr < n) {
-			upIndices[ptr] = up.size();
-			up.add(getNodeNameFor(ptr));
+			upIndices[ptr] = upNodes.size();
+			upNodes.add(getNodeNameFor(ptr));
+			upEdges.add(getEdgeNameFor(ptr, true));
 			ptr = sent.governor(ptr);
 		}
 		
 		// the order down starts out backwards
-		down = new ArrayList<String>();
-		ptr = end;
-		while(ptr >= 0 && ptr < n && upIndices[ptr] < 0) {
-			down.add(getNodeNameFor(ptr));
-			ptr = sent.governor(ptr);
+		if(!toRoot) {
+			downNodes = new ArrayList<String>();
+			downEdges = new ArrayList<String>();
+			ptr = end;
+			while(ptr >= 0 && ptr < n && upIndices[ptr] < 0) {
+				downNodes.add(getNodeNameFor(ptr));
+				downEdges.add(getEdgeNameFor(ptr, false));
+				ptr = sent.governor(ptr);
+			}
+			Collections.reverse(downNodes);
+			Collections.reverse(downEdges);
 		}
-		Collections.reverse(down);
 
 		if(ptr >= 0 && ptr < n) {
 			connected = true;
 			top = getNodeNameFor(ptr);
-			up = up.subList(0, upIndices[ptr]);	// trim up at the point where the two paths meet
+			upNodes = upNodes.subList(0, upIndices[ptr]);	// trim up at the point where the two paths meet
+			upEdges = upEdges.subList(0, upIndices[ptr]);
 		}
 		else {
 			connected = false;
-			top = "<unconnected>";
+			top = "ROOT";
 		}
 	}
 	
 	private String getNodeNameFor(int i) {
-		if(ruleType == RuleType.DEP) return sent.dependencyType(i);
-		else if(ruleType == RuleType.LEMMA) return sent.getLemma(i);
-		else if(ruleType == RuleType.POS) return sent.getPos(i);
-		else if(ruleType == RuleType.NONE) return "*";
+		if(nodeType == NodeType.LEMMA) return sent.getLemma(i);
+		else if(nodeType == NodeType.POS) return sent.getPos(i);
+		else if(nodeType == NodeType.NONE) return "*";
 		else throw new RuntimeException();
 	}
 	
-	public boolean isConnected() { return connected; }
+	private String getEdgeNameFor(int i, boolean goingUp) {
+		if(edgeType == EdgeType.DEP) return sent.dependencyType(i);
+		else if(edgeType == EdgeType.DIRECTION) return goingUp ? "<" : ">";
+		else throw new RuntimeException();
+	}
 	
-	public RuleType getRuleType() { return ruleType; }
+	/** returns true if represents a path from a token to the root of the sentence */
+	public boolean toRoot() { return toRoot; }
+
+	public boolean isConnected() {
+		if(toRoot) {
+			assert false : "trivially true";
+			return true;
+		}
+		return connected;
+	}
+	
+	public NodeType getNodeType() { return nodeType; }
+	
+	public EdgeType getEdgeType() { return edgeType; }
 	
 	public int getStart() { return start; }
 	
-	public int getEnd() { return end; }
+	public int getEnd() {
+		if(toRoot)
+			throw new RuntimeException("not defined");
+		return end;
+	}
 	
 	private String path;
 	public String getPath() {
 		if(path == null) {
 			StringBuilder sb = new StringBuilder();
-			for(String s : up) {
-				sb.append(s);
-				sb.append("<");
+			for(int i=0; i<upNodes.size(); i++) {
+				sb.append(upNodes.get(i));
+				sb.append(upEdges.get(i));
 			}
 			sb.append(top);
-			for(String s : down) {
-				sb.append(">");
-				sb.append(s);
+			if(!toRoot) {
+				for(int i=0; i<downNodes.size(); i++) {
+					sb.append(downEdges.get(i));
+					sb.append(downNodes.get(i));
+				}
 			}
 			path = sb.toString();
 		}
 		return path;
+	}
+	
+	/** prefix may be null */
+	public void pathNGrams(int length, Collection<String> addTo, String prefix) {
+		int U = 2*upNodes.size() + 1;
+		if(!toRoot) U += 2*downNodes.size();
+		for(int s=0; s < U-length+1; s++) {
+			StringBuilder ngram = new StringBuilder();
+			if(prefix != null)
+				ngram.append(prefix);
+			for(int i=0; i<length; i++)
+				ngram.append(pathNGramsHelper2(s + i));
+			addTo.add(ngram.toString());
+		}
+	}
+	private String pathNGramsHelper2(int i) {
+		// 012345678901234
+		//  < < < < > > >
+		// A B C D R X Y Z
+		// 0 1 2 3   0 1 2
+		int U = 2 * upNodes.size();	// U=8 in example above
+		if(i < U) {
+			if(i % 2 == 0) return upNodes.get(i / 2);
+			else return upEdges.get(i / 2);
+		}
+		else if(i == U) return "ROOT";
+		else {
+			assert !toRoot;
+			int j = i - U;
+			if(j % 2 == 0) return downNodes.get((j / 2) - 1);
+			else return downEdges.get(j / 2);
+		}
 	}
 	
 	@Override
--- src/test/java/edu/jhu/hlt/fnparse/features/PathTests.java
+++ src/test/java/edu/jhu/hlt/fnparse/features/PathTests.java
@@ -1,7 +1,8 @@
 package edu.jhu.hlt.fnparse.features;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,7 +10,8 @@ import org.junit.Test;
 
 import edu.jhu.hlt.fnparse.datatypes.FNParse;
 import edu.jhu.hlt.fnparse.datatypes.Sentence;
-import edu.jhu.hlt.fnparse.features.Path.RuleType;
+import edu.jhu.hlt.fnparse.features.Path.EdgeType;
+import edu.jhu.hlt.fnparse.features.Path.NodeType;
 import edu.jhu.hlt.fnparse.inference.ParserTests;
 
 public class PathTests {
@@ -21,9 +23,26 @@ public class PathTests {
 		List<String> toks = Arrays.asList(sent.getWords());
 		int start = toks.indexOf("fox");
 		int end = toks.indexOf("fence");
-		Path p = new Path(sent, start, end, RuleType.LEMMA);
+		Path p = new Path(sent, start, end, NodeType.LEMMA, EdgeType.DIRECTION);
 		System.out.println(p);
 		assertEquals("fox<jump>over>fence", p.toString());
+		
+		
+		int head = toks.indexOf("jumped");
+		p = new Path(sent, head, NodeType.POS, EdgeType.DEP);
+		System.out.println(p);
+
+		
+		head = toks.indexOf("the");
+		p = new Path(sent, head, NodeType.LEMMA, EdgeType.DIRECTION);
+		List<String> ngrams = new ArrayList<String>();
+		p.pathNGrams(5, ngrams, "path5grams-");
+		System.out.println(ngrams);
+		
+		p = new Path(sent, start, end, NodeType.LEMMA, EdgeType.DIRECTION);
+		ngrams.clear();
+		p.pathNGrams(5, ngrams, null);
+		System.out.println(ngrams);
 	}
 
 }
