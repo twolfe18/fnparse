05ee098991a92d39932bf96a098c574b67ac153b
Travis Wolfe <twolfe18@gmail.com>
2014-09-17 11:43:44 -0400

    Frames now have information about the types of their roles (e.g. are they core or not)

--- src/main/java/edu/jhu/hlt/fnparse/data/FrameIndex.java
+++ src/main/java/edu/jhu/hlt/fnparse/data/FrameIndex.java
@@ -1,10 +1,20 @@
 package edu.jhu.hlt.fnparse.data;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.*;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
+import org.apache.log4j.Logger;
 
 import edu.jhu.hlt.fnparse.datatypes.Frame;
 import edu.jhu.hlt.fnparse.datatypes.LexicalUnit;
@@ -12,12 +22,14 @@ import edu.jhu.hlt.fnparse.datatypes.LexicalUnit;
 /**
  * Reads frames from disk and provides access to them
  */
-public class FrameIndex implements FrameIndexInterface{
+public class FrameIndex implements FrameIndexInterface {
+	public static final Logger LOG = Logger.getLogger(FrameIndex.class);
 	/**
 	 * Frame used to indicate that a word does not evoke a frame
 	 */
 	public static final Frame nullFrame = Frame.nullFrame;
 	public static final int framesInFrameNet = 1019;	// The number of frames in Framenet 1.5
+
 	public static class FrameIndexIterator implements Iterable<Frame>{
 		// The reader that points to the frameindex file
 		private LineIterator litFE; 
@@ -49,7 +61,7 @@ public class FrameIndex implements FrameIndexInterface{
 				assert curFrameIDFE.equals(curFrameIDLU);
 			} catch (IOException e) {
 				throw new RuntimeException(e);
-			}	
+			}
 		}
 		@Override
 		public Iterator<Frame> iterator() {
@@ -76,7 +88,11 @@ public class FrameIndex implements FrameIndexInterface{
 						// read lines till the curFrameID is same as prevFrameId (or prevFrameID is null)
 						// and we can still read lines
 						String[] l = curLineFE.split("\t");
-						fename.add(l[4]);
+
+						// Roles can have quotes on them, remove them
+						String roleName = l[4].replaceAll("(^\")|(\"$)", "");
+
+						fename.add(roleName);
 						curLineFE = litFE.nextLine();
 						l = curLineFE.split("\t");
 						curFrameIDFE = l[0];
@@ -109,23 +125,24 @@ public class FrameIndex implements FrameIndexInterface{
 
 				@Override
 				public void remove() {
-					throw new UnsupportedOperationException(); 	
+					throw new UnsupportedOperationException(); 
 				}
 			};
 			return it;
 		}
 	}
-	
+
 	private static FrameIndex singleton;
 	private List<Frame> allFrames = new ArrayList<Frame>(framesInFrameNet);;
 	private Map<String, Frame> nameToFrameMap = new HashMap<String, Frame>();
 	private Map<Integer, String> indexToNameMap = new HashMap<Integer, String>();
 	private Frame[] byId = new Frame[framesInFrameNet + 1];
-	
+
 	private FrameIndex() { 
 		// singleton
 		// Since its a singleton and its really lightweight.
-		// Just populate it during cosntruction.
+		// Just populate it during construction.
+		LOG.info("reading frames");
 		int idx = 0;
 		for(Frame f: new FrameIndexIterator()){
 			allFrames.add(f);
@@ -134,7 +151,30 @@ public class FrameIndex implements FrameIndexInterface{
 			assert byId[f.getId()] == null;
 			byId[f.getId()] = f;
 		}
-	}	
+
+		// Read in information about what the core roles are
+		try {
+			File coreFile = new File("toydata/core-roles.txt");
+			LOG.info("reading role core types from " + coreFile.getPath());
+			BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(coreFile)));
+			while (r.ready()) {
+				String line = r.readLine();
+				String[] toks = line.split("\\t");
+				assert toks.length == 3;
+				Frame f = nameToFrameMap.get(toks[0]);
+				int k = Arrays.asList(f.getRoles()).indexOf(toks[1]);
+				if (k < 0) {
+					LOG.warn("missing " + toks[1] + " role for " + toks[0] + "?");
+				} else {
+					f.setRoleType(k, toks[2]);
+				}
+			}
+			r.close();
+			LOG.info("done reading role core types");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public static FrameIndex getInstance() {
 		if(singleton == null)
@@ -142,7 +182,6 @@ public class FrameIndex implements FrameIndexInterface{
 		return singleton;
 	}
 
-	
 	public Frame getFrame(int id) {
 		Frame f = byId[id];
 		if(f == null)
@@ -153,14 +192,14 @@ public class FrameIndex implements FrameIndexInterface{
 	public Frame getFrame(String name) {
 		return nameToFrameMap.get(name);
 	}
-	
+
 	public int getRoleIdx(Frame f, String roleName) {
 		for(int i=0; i<f.numRoles(); i++)
 			if(f.getRole(i).equals(roleName))
 				return i;
 		throw new IllegalStateException("frame=" + f + ", roleName=" + roleName);
 	}
-	
+
 	public List<Frame> allFrames() {
 		return allFrames;
 	}
