564641ea67c0c9d6539371fbd7d9f244948e4f35
Travis Wolfe <twolfe18@gmail.com>
2014-09-30 14:55:27 -0400

    Ran experiment to show that there are both roles that typically have both very long and very short argument spans.
    This may indicate that splitting the model for role types is a good idea:
    roles that typically get long arguments get constituency-based latent syntax
    roles that typically get short arguments get dependency-based latent syntax.
    
    Before doing this, I should run both versions of latent syntax and see report
    the accuracy for these role types to confirm this intuition before implementing
    the split-logic and training.
    
    frame-roles with shortes arguments:
     1 <ArgWithWidth Weapon.Weapon N=825 99.9%<5w 99.9%<10w> -1309.00
     2 <ArgWithWidth Locale_by_use.Locale N=423 98.8%<5w 99.1%<10w> -818.00
     3 <ArgWithWidth Political_locales.Locale N=408 100.0%<5w 100.0%<10w> -811.00
     4 <ArgWithWidth Leadership.Leader N=322 95.7%<5w 99.4%<10w> -531.00
     5 <ArgWithWidth Cardinal_numbers.Number N=226 100.0%<5w 100.0%<10w> -450.00
     6 <ArgWithWidth Substance.Substance N=226 100.0%<5w 100.0%<10w> -450.00
     7 <ArgWithWidth Quantity.Quantity N=224 100.0%<5w 100.0%<10w> -423.00
     8 <ArgWithWidth Locative_relation.Ground N=401 80.3%<5w 95.5%<10w> -341.00
     9 <ArgWithWidth Buildings.Building N=157 100.0%<5w 100.0%<10w> -314.00
     10 <ArgWithWidth Statement.Speaker N=317 76.0%<5w 93.4%<10w> -286.00
     11 <ArgWithWidth Calendric_unit.Unit N=140 100.0%<5w 100.0%<10w> -279.00
     12 <ArgWithWidth Aggregate.Aggregate N=141 99.3%<5w 100.0%<10w> -277.00
     13 <ArgWithWidth Natural_features.Locale N=139 100.0%<5w 100.0%<10w> -277.00
     14 <ArgWithWidth Cardinal_numbers.Entity N=183 89.6%<5w 98.9%<10w> -270.00
     15 <ArgWithWidth Origin.Origin N=150 96.7%<5w 98.7%<10w> -270.00
     16 <ArgWithWidth Leadership.Governed N=191 89.5%<5w 95.8%<10w> -256.00
     17 <ArgWithWidth Locale.Locale N=129 99.2%<5w 100.0%<10w> -255.00
     18 <ArgWithWidth Origin.Entity N=151 95.4%<5w 99.3%<10w> -237.00
     19 <ArgWithWidth People.Person N=121 99.2%<5w 100.0%<10w> -236.00
     20 <ArgWithWidth Capability.Entity N=190 90.0%<5w 97.4%<10w> -234.00
    
    frame-roles with longest arguments:
     2884 <ArgWithWidth Statement.Message N=386 11.7%<5w 30.8%<10w> 544.00
     2883 <ArgWithWidth Temporal_collocation.Trajector_event N=159 23.3%<5w 59.7%<10w> 147.00
     2882 <ArgWithWidth Inclusion.Part N=99 18.2%<5w 41.4%<10w> 121.00
     2881 <ArgWithWidth Time_vector.Event N=121 24.8%<5w 61.2%<10w> 115.00
     2880 <ArgWithWidth Capability.Event N=210 39.5%<5w 69.5%<10w> 91.00
     2879 <ArgWithWidth Likelihood.Hypothetical_event N=108 24.1%<5w 59.3%<10w> 88.00
     2878 <ArgWithWidth Evidence.Proposition N=66 13.6%<5w 43.9%<10w> 84.00
     2877 <ArgWithWidth Causation.Effect N=96 28.1%<5w 63.5%<10w> 82.00
     2876 <ArgWithWidth Possession.Possession N=147 41.5%<5w 67.3%<10w> 71.00
     2875 <ArgWithWidth Attributed_information.Proposition N=41 7.3%<5w 26.8%<10w> 70.00
     2874 <ArgWithWidth Awareness.Content N=82 26.8%<5w 62.2%<10w> 66.00
     2873 <ArgWithWidth Existence.Entity N=92 31.5%<5w 59.8%<10w> 65.00
     2872 <ArgWithWidth Attempt.Goal N=69 27.5%<5w 59.4%<10w> 62.00
     2871 <ArgWithWidth Unattributed_information.Reported_fact N=47 19.1%<5w 44.7%<10w> 58.00
     2870 <ArgWithWidth Using.Purpose N=101 39.6%<5w 73.3%<10w> 57.00
     2869 <ArgWithWidth Desiring.Event N=50 22.0%<5w 54.0%<10w> 55.00
     2868 <ArgWithWidth Telling.Message N=31 6.5%<5w 12.9%<10w> 52.00
     2867 <ArgWithWidth Causation.Cause N=88 30.7%<5w 62.5%<10w> 51.00
     2866 <ArgWithWidth Activity_start.Activity N=54 27.8%<5w 63.0%<10w> 44.00
     2865 <ArgWithWidth Certainty.Content N=36 16.7%<5w 44.4%<10w> 43.00

--- src/main/java/edu/jhu/hlt/fnparse/experiment/WideArguments.java
+++ src/main/java/edu/jhu/hlt/fnparse/experiment/WideArguments.java
@@ -1,9 +1,16 @@
 package edu.jhu.hlt.fnparse.experiment;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
+import edu.jhu.hlt.fnparse.data.DataUtil;
 import edu.jhu.hlt.fnparse.data.FileFrameInstanceProvider;
+import edu.jhu.hlt.fnparse.data.FrameIndex;
 import edu.jhu.hlt.fnparse.datatypes.FNParse;
+import edu.jhu.hlt.fnparse.datatypes.Frame;
 import edu.jhu.hlt.fnparse.datatypes.FrameInstance;
 import edu.jhu.hlt.fnparse.datatypes.Sentence;
 import edu.jhu.hlt.fnparse.datatypes.Span;
@@ -21,8 +28,131 @@ import edu.jhu.hlt.fnparse.inference.heads.SemaforicHeadFinder;
  * @author travis
  */
 public class WideArguments {
-	
+
 	public static void main(String[] args) {
+		//showWideArgs();
+		measureArgWidth();
+	}
+
+	public static class ArgWithWidth {
+		public static final int MAX_ARG_WIDTH = 12;
+		private Frame frame;
+		private String role;
+		private int[] argWidths;
+		private int totalObservations;
+
+		public ArgWithWidth(Frame f, int role) {
+			this(f, f.getRole(role));
+		}
+
+		public ArgWithWidth(Frame f, String role) {
+			this.frame = f;
+			this.role = role;
+			this.argWidths = new int[MAX_ARG_WIDTH + 2];
+		}
+
+		@Override
+		public String toString() {
+			return String.format("<ArgWithWidth %s.%s N=%d %.1f%%<5w %.1f%%<10w>",
+					frame.getName(), role,
+					totalObservations,
+					100d * proportionShorterThan(5),
+					100d * proportionShorterThan(10));
+		}
+
+		public void observeArgument(Span s) {
+			totalObservations++;
+			int w = s.width();
+			if (w < MAX_ARG_WIDTH) {
+				argWidths[w]++;
+			} else {
+				argWidths[argWidths.length - 1]++;
+			}
+		}
+
+		public double proportionShorterThan(int width) {
+			int n = 0, d = 0;
+			for (int i = 0; i < argWidths.length; i++) {
+				d += argWidths[i];
+				if (i < width)
+					n += argWidths[i];
+			}
+			return ((double) n) / d;
+		}
+
+		public double widthScore() {
+			double s = 0d;
+			for (int i = 0; i < argWidths.length; i++)
+				s += argWidths[i] * widthScore(i);
+			return s;
+		}
+
+		public static int widthScore(int width) {
+			if (width == 1)
+				return -2;
+			if (width <= 3)
+				return -1;
+			if (width <= 5)
+				return 0;
+			if (width <= 8)
+				return 1;
+			else
+				return 2;
+		}
+	}
+
+	public static void measureArgWidth() {
+		long start = System.currentTimeMillis();
+		List<ArgWithWidth> args = new ArrayList<>();
+		ArgWithWidth[][] widths =
+				new ArgWithWidth[FrameIndex.framesInFrameNet + 1][100];
+		for (FNParse p : DataUtil.iter2list(FileFrameInstanceProvider.dipanjantrainFIP.getParsedSentences())) {
+			for (FrameInstance fi : p.getFrameInstances()) {
+				int f = fi.getFrame().getId();
+				for (int k = 0; k < fi.numArguments(); k++) {
+					Span s = fi.getArgument(k);
+					if (s == Span.nullSpan)
+						continue;
+					ArgWithWidth w = widths[f][k];
+					if (w == null) {
+						w = new ArgWithWidth(fi.getFrame(), k);
+						widths[f][k] = w;
+						args.add(w);
+					}
+					w.observeArgument(s);
+				}
+			}
+		}
+
+		// Sort roles by their width distribution
+		Collections.sort(args, new Comparator<ArgWithWidth>() {
+			@Override
+			public int compare(ArgWithWidth o1, ArgWithWidth o2) {
+				double w = o1.widthScore() - o2.widthScore();
+				if (w < 0d) return -1;
+				if (w > 0d) return 1;
+				return 0;
+			}
+		});
+
+		// Print out the top and bottom of the list
+		int k = 20;
+		System.out.println("frame-roles with shortes arguments:");
+		for (int i = 0; i < k; i++) {
+			ArgWithWidth a = args.get(i);
+			System.out.printf("% 2d %-25s %.2f\n", i+1, a, a.widthScore());
+		}
+		System.out.println("frame-roles with longest arguments:");
+		for (int i = 0; i < k; i++) {
+			int j = (args.size() - i) - 1;
+			ArgWithWidth a = args.get(j);
+			System.out.printf("% 2d %-25s %.2f\n", j+1, a, a.widthScore());
+		}
+		System.out.printf("done, took %.1f seconds\n",
+				(System.currentTimeMillis() - start) / 1000d);
+	}
+
+	public static void showWideArgs() {
 		int total = 0, wide = 0;
 		HeadFinder hf = new SemaforicHeadFinder();
 		Iterator<FNParse> iter = FileFrameInstanceProvider.dipanjantrainFIP.getParsedSentences();
