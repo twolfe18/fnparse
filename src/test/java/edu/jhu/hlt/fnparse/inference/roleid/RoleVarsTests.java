612ffa38364407c1b39b6b5b7243302d9d951e0e
Travis Wolfe <twolfe18@gmail.com>
2014-10-29 14:06:21 -0400

    updated rvar iter test

--- src/test/java/edu/jhu/hlt/fnparse/inference/roleid/RoleVarsTests.java
+++ src/test/java/edu/jhu/hlt/fnparse/inference/roleid/RoleVarsTests.java
@@ -19,45 +19,50 @@ import edu.jhu.hlt.fnparse.inference.role.head.RoleHeadVars.RVar;
 
 public class RoleVarsTests {
 
-	private FNParse parse = ParserTests.makeDummyParse();
-	private ParserParams params = new ParserParams();
-	private RoleHeadStage.Params roleIdParams = new RoleHeadStage.Params(params);
-	
-	@Test
-	public void testIterator() {
-		testIterator(parse);
-	}
+  private FNParse parse = ParserTests.makeDummyParse();
+  private ParserParams params = new ParserParams();
+  private RoleHeadStage.Params roleIdParams = new RoleHeadStage.Params(params);
 
-	private void testIterator(FNParse parse) {
-		final int n = parse.getSentence().size();
-		for(FrameInstance fi : parse.getFrameInstances()) {
-			System.out.println(fi);
-			int targetHead = fi.getTarget().start;
-			RoleHeadVars rv = new RoleHeadVars(targetHead, fi.getFrame(), parse.getSentence(), params, roleIdParams);
+  @Test
+  public void testIterator() {
+    testIterator(parse);
+  }
 
-			Var[][] vars = rv.r_kj;
-			assertEquals(vars.length, fi.getFrame().numRoles());
-			
-			int nnn = 0;
-			for(int i=0; i<vars.length; i++)
-				for(int j=0; j<vars[i].length; j++)
-					if(vars[i][j] != null)
-						nnn++;
+  private void testIterator(FNParse parse) {
+    final int n = parse.getSentence().size();
+    for(FrameInstance fi : parse.getFrameInstances()) {
+      System.out.println(fi);
+      int targetHead = fi.getTarget().start;
+      RoleHeadVars rv = new RoleHeadVars(targetHead, fi.getFrame(), parse.getSentence(), params, roleIdParams);
 
-			int iter_nnn = 0;
-			Iterator<RVar> iter = rv.getVars();
-			assertTrue(iter.hasNext());
-			while(iter.hasNext()) {
-				RVar r = iter.next();
-				System.out.println(r);
-				assertEquals(r.roleVar.getNumStates(), 2);
-				assertEquals(r.roleVar.getType(), VarType.PREDICTED);
-				assertEquals(vars[r.k][r.j], r.roleVar);
-				assertTrue(r.j <= n && r.j >= 0);
-				assertTrue(r.k >= 0 && r.k < fi.getFrame().numRoles());
-				iter_nnn++;
-			}
-			assertEquals(nnn, iter_nnn);
-		}
-	}
+      Var[][] vars = rv.r_kj;
+      assertEquals(vars.length, fi.getFrame().numRoles());
+
+      int nnn = 0;
+      for (int i=0; i<vars.length; i++)
+        for (int j=0; vars[i] != null && j<vars[i].length; j++)
+          if (vars[i][j] != null)
+            nnn++;
+
+      int iter_nnn = 0;
+      Iterator<RVar> iter = rv.getVars();
+      assertTrue(iter.hasNext());
+      int prev_j = -1, prev_k = -1;
+      while (iter.hasNext()) {
+        RVar r = iter.next();
+        System.out.println(r);
+        assertEquals(r.roleVar.getNumStates(), 2);
+        assertEquals(r.roleVar.getType(), VarType.PREDICTED);
+        assertEquals(vars[r.k][r.j], r.roleVar);
+        assertTrue(r.j <= n && r.j >= 0);
+        assertTrue(r.k >= 0 && r.k < fi.getFrame().numRoles());
+        if (prev_j >= 0)
+          assertTrue(r.j != prev_j || r.k != prev_k);
+        prev_j = r.j;
+        prev_k = r.k;
+        iter_nnn++;
+      }
+      assertEquals(nnn, iter_nnn);
+    }
+  }
 }
