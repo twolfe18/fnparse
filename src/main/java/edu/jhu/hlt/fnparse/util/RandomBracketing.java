1565c49bed0e0b79d216d57eca2c341582a81f13
Travis Wolfe <twolfe18@gmail.com>
2014-12-16 15:44:48 -0500

    added RandomBracketing -- didn't mean to commit everything in that last one

---
+++ src/main/java/edu/jhu/hlt/fnparse/util/RandomBracketing.java
@@ -0,0 +1,44 @@
+package edu.jhu.hlt.fnparse.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+import edu.jhu.hlt.fnparse.datatypes.Span;
+
+/**
+ * Makes random bracketings in a bottom up fashion. Useful for
+ * comparison to latent syntax (which should have statistically
+ * significantly higher recall).
+ * 
+ * @author travis
+ */
+public class RandomBracketing {
+  private Random rand;
+
+  public RandomBracketing(Random r) {
+    this.rand = r;
+  }
+  
+  public void bracket(int n, Collection<Span> addTo) {
+    List<Span> fronteer = new ArrayList<>();
+    // initialize as all preterminals
+    for (int i = 0; i < n; i++) {
+      fronteer.add(Span.widthOne(i));
+      addTo.add(Span.widthOne(i));
+    }
+    while (fronteer.size() > 1) {
+      // merge (i,i+1) for some random i
+      int i = rand.nextInt(fronteer.size() - 1);
+      Span l = fronteer.get(i);
+      Span r = fronteer.get(i + 1);
+      Span merged = Span.getSpan(l.start, r.end);
+      // this preserves the ordering of spans
+      fronteer.set(i, merged);
+      fronteer.remove(i + 1);
+      addTo.add(merged);
+    }
+    addTo.add(fronteer.get(0));
+  }
+}
