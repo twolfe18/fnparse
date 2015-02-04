1b5f174960f77dd7e8806c786e02c7a207f6fb29
Travis Wolfe <twolfe18@gmail.com>
2015-02-04 13:12:09 -0500

    added class to tell when a specified length of time has passed, slightly different from Timer

---
+++ src/main/java/edu/jhu/hlt/fnparse/util/TimeMarker.java
@@ -0,0 +1,29 @@
+package edu.jhu.hlt.fnparse.util;
+
+/**
+ * Class designed to answer the question:
+ * "has it been X seconds since I called you last?"
+ *
+ * @author travis
+ */
+public class TimeMarker {
+  private long lastMark = -1;
+  /**
+   * @return true if enoughSeconds have passed since this this method last
+   * returned true, or if this method has never been called.
+   */
+  public boolean enoughTimePassed(double enoughSeconds) {
+    if (lastMark < 0) {
+      lastMark = System.currentTimeMillis();
+      return true;
+    }
+    long time = System.currentTimeMillis();
+    double elapsed = (time - lastMark) / 1000d;
+    if (elapsed >= enoughSeconds) {
+      lastMark = time;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
