cb7474260268b4d929569b937f0ffbd02eb6b87a
Travis Wolfe <twolfe18@gmail.com>
2015-09-02 16:33:35 -0400

    propbank train server/client scripts now support running from "mvn exec:java" as well as jar

--- scripts/propbank-train-client.sh
+++ scripts/propbank-train-client.sh
@@ -11,10 +11,10 @@ echo "java: `java -version 2>&1`"
 echo "java versions: `which -a java`"
 echo "arguments: $@"
 
-set -euo pipefail
-
 # Will send an average to the param server every this many seconds
-SAVE_INTERVAL=300
+if [[ $SAVE_INTERVAL == "" ]]; then
+  SAVE_INTERVAL=300
+fi
 
 if [[ $# != 9 ]]; then
   echo "please provide"
@@ -28,6 +28,7 @@ if [[ $# != 9 ]]; then
   echo "7) a classpath with all dependencies"
   echo "8) a file containing the feature set"
   echo "9) feature mode, i.e. \"LOCAL\", \"ARG-LOCATION\", \"NUM-ARGS\", \"ROLE-COOC\", \"FULL\""
+  echo "NOTE: You can run with RUN_WITH_MAVEN=true (which ignores classpath) or just leave it off (for java+classpath)"
   exit -1
 fi
 
@@ -37,50 +38,50 @@ PARAM_SERVER_HOST=$3
 PARAM_SERVER_PORT=$4
 i=$5
 NUM_SHARD=$6
-#JAR=$7
 CP=$7
 FEATURES=$8
 FEATURE_MODE=$9
 
-#echo "copying jar file to the working directory:"
-#echo "$JAR  =>  $WORKING_DIR/fnparse.jar"
-#cp $JAR $WORKING_DIR/fnparse.jar
-#cp $FEATURES $WORKING_DIR/features.txt
-
-#CP=`find target/ -name '*.jar' | tr '\n' ':'`
-#CP=$WORKING_DIR/fnparse.jar
-echo "starting with CP=$CP"
-
-java \
-  -Ddata.wordnet=toydata/wordnet/dict \
+COMMAND="-Ddata.wordnet=toydata/wordnet/dict \
   -Ddata.embeddings=data/embeddings \
   -Ddata.ontonotes5=data/ontonotes-release-5.0/LDC2013T19/data/files/data/english/annotations \
   -Ddata.propbank.conll=../conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0/data \
   -Ddata.propbank.frames=data/ontonotes-release-5.0-fixed-frames/frames \
   -DdisallowConcreteStanford=false \
-  -XX:+UseSerialGC -Xmx12G -ea -server -cp ${CP} \
-  edu.jhu.hlt.fnparse.rl.rerank.RerankerTrainer \
-  "workerJob$i" \
-  workingDir ${WORKING_DIR} \
-  parallelLearnDebug true \
-  numClientsForParamAvg ${NUM_SHARD} \
-  paramServerHost ${PARAM_SERVER_HOST} \
-  paramServerPort ${PARAM_SERVER_PORT} \
-  isParamServer false \
-  numShards ${NUM_SHARD} \
-  shard ${i} \
-  paramAvgSecBetweenSaves ${SAVE_INTERVAL} \
-  redis.host.propbankParses ${PARSE_SERVER} \
-  redis.port.propbankParses 6379 \
-  redis.db.propbankParses 0 \
-  addStanfordParses false \
-  realTestSet false \
-  propbank true \
-  l2Penalty 1e-8 \
-  globalL2Penalty 1e-7 \
-  secsBetweenShowingWeights 180 \
-  trainTimeLimit 360 \
-  featureSetFile ${FEATURES} \
-  featureMode ${FEATURE_MODE} \
-  featCoversFrames false
+  -DworkingDir=${WORKING_DIR} \
+  -DparallelLearnDebug=true \
+  -DnumClientsForParamAvg=${NUM_SHARD} \
+  -DparamServerHost=${PARAM_SERVER_HOST} \
+  -DparamServerPort=${PARAM_SERVER_PORT} \
+  -DisParamServer=false \
+  -DnumShards=${NUM_SHARD} \
+  -Dshard=${i} \
+  -DparamAvgSecBetweenSaves=${SAVE_INTERVAL} \
+  -Dredis.host.propbankParses=${PARSE_SERVER} \
+  -Dredis.port.propbankParses=6379 \
+  -Dredis.db.propbankParses=0 \
+  -DaddStanfordParses=false \
+  -DrealTestSet=false \
+  -Dpropbank=true \
+  -Dl2Penalty=1e-8 \
+  -DglobalL2Penalty=1e-7 \
+  -DsecsBetweenShowingWeights=180 \
+  -DtrainTimeLimit=360 \
+  -DfeatureSetFile=${FEATURES} \
+  -DfeatureMode=${FEATURE_MODE} \
+  -DfeatCoversFrames=false"
+
+if [[ $RUN_WITH_MAVEN == "true" ]]; then
+  echo "Running with maven, no classpath needed..."
+  COMMAND="mvn compile exec:java \
+    -Dexec.mainClass=edu.jhu.hlt.fnparse.rl.rerank.RerankerTrainer \
+    -Dexec.args=workerJob$i \
+    $COMMAND"
+  MAVEN_OPTS="-XX:+UseSerialGC -Xmx7G -ea -server" $COMMAND
+else
+  echo "Running with java, classpath: $CP"
+  COMMAND="java $COMMAND -XX:+UseSerialGC -Xmx12G -ea -server -cp ${CP} \
+    edu.jhu.hlt.fnparse.rl.rerank.RerankerTrainer \"workerJob$i\""
+  exec $COMMAND
+fi
 
