e23ab24767450ec4ac3f2c44892502ad0483c722
Travis Wolfe <twolfe18@gmail.com>
2015-09-16 15:19:22 -0400

    updated merge-two-shards.sh, but Im going to delete this soon

--- scripts/precompute-features/merge-two-shards.sh
+++ scripts/precompute-features/merge-two-shards.sh
@@ -28,12 +28,38 @@ JOB_DIR_B=$2
 OUTPUT_DIR=$3
 JAR=$4
 
-java -Xmx15G -ea -server -cp $JAR \
-  -DfeatIn1=$JOB_DIR_A/features.txt.gz \
-  -DalphIn1=$JOB_DIR_A/template-feat-indices.txt.gz \
-  -DfeatIn2=$JOB_DIR_B/features.txt.gz \
-  -DalphIn2=$JOB_DIR_B/template-feat-indices.txt.gz \
-  -DfeatOut=$OUTPUT_DIR/features.txt.gz \
-  -DalphOut=$OUTPUT_DIR/template-feat-indices.txt.gz \
+# Old version that does extra feature copying
+#java -Xmx15G -ea -server -cp $JAR \
+#  -DfeatIn1=$JOB_DIR_A/features.txt.gz \
+#  -DalphIn1=$JOB_DIR_A/template-feat-indices.txt.gz \
+#  -DfeatIn2=$JOB_DIR_B/features.txt.gz \
+#  -DalphIn2=$JOB_DIR_B/template-feat-indices.txt.gz \
+#  -DfeatOut=$OUTPUT_DIR/features.txt.gz \
+#  -DalphOut=$OUTPUT_DIR/template-feat-indices.txt.gz \
+#  edu.jhu.hlt.fnparse.features.precompute.AlphabetMerger
+
+# Sort the incoming alphabets
+A1=$JOB_DIR_A/template-feat-indices.txt.gz
+A2=$JOB_DIR_B/template-feat-indices.txt.gz
+
+A1_SORTED=$JOB_DIR_A/template-feat-indices.sorted.txt
+A2_SORTED=$JOB_DIR_B/template-feat-indices.sorted.txt
+
+echo "`date` sorting $A1  =>  $A1_SORTED"
+LC_ALL=C sort -t '	' -k 3,4 -o $A1_SORTED <$(zcat $A1 | tail -n+2)
+
+echo "`date` sorting $A2  =>  $A2_SORTED"
+LC_ALL=C sort -t '	' -k 3,4 -o $A2_SORTED <$(zcat $A2 | tail -n+2)
+
+A1_OUTPUT=$JOB_DIR_A/template-feat-indices.sorted.txt
+A2_OUTPUT=$JOB_DIR_B/template-feat-indices.sorted.txt
+
+echo "`date` done sorting, about to merge"
+java -Xmx1G -ea -server -cp $JAR \
+  -DinAlph1=$A1_SORTED \
+  -DinAlph2=$A2_SORTED \
+  -DoutAlph1=$OUTPUT_DIR/template-feat-indices.sorted.txt \
   edu.jhu.hlt.fnparse.features.precompute.AlphabetMerger
+
+echo "`date` done"
 
