4190e0669575c6db9f36f9cc8fd808270bb64ec6
Travis Wolfe <twolfe18@gmail.com>
2015-10-15 12:14:59 -0400

    added "run on all lines" mehtod for LineByLine

--- src/main/java/edu/jhu/hlt/fnparse/util/LineByLine.java
+++ src/main/java/edu/jhu/hlt/fnparse/util/LineByLine.java
@@ -3,6 +3,13 @@ package edu.jhu.hlt.fnparse.util;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import edu.jhu.hlt.tutils.FileUtil;
 import edu.jhu.hlt.tutils.Log;
@@ -19,11 +26,30 @@ public interface LineByLine {
         observeLine(line);
         if (tm.enoughTimePassed(15)) {
           Log.info("processed " + tm.numMarks()
-              + " lines in " + tm.secondsSinceFirstMark() + " seconds, "
-              + Describe.memoryUsage());
+            + " lines in " + tm.secondsSinceFirstMark() + " seconds, "
+            + Describe.memoryUsage());
         }
       }
     }
   }
 
+  /** glob should look like "glob:**\/*" -- without the escape backslash of course */
+  default public void runManyFiles(String glob, File parent) throws IOException {
+    PathMatcher pm = FileSystems.getDefault().getPathMatcher(glob);
+    Files.walkFileTree(parent.toPath(), new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+        if (pm.matches(path)) {
+          File f = path.toFile();
+          Log.info("reading: " + f.getPath() + "\t" + Describe.memoryUsage());
+          LineByLine.this.run(f);
+        }
+        return FileVisitResult.CONTINUE;
+      }
+      @Override
+      public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+      }
+    });
+  }
 }
