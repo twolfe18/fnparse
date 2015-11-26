4973ade913a704cada162a5c3f93332b345a0cdf
Travis Wolfe <twolfe18@gmail.com>
2015-11-25 21:45:35 -0500

    implemented Propbank FNParse role flattening

--- src/main/java/edu/jhu/hlt/fnparse/data/propbank/PropbankContRefRoleFNParseConverter.java
+++ src/main/java/edu/jhu/hlt/fnparse/data/propbank/PropbankContRefRoleFNParseConverter.java
@@ -1,7 +1,14 @@
 package edu.jhu.hlt.fnparse.data.propbank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import edu.jhu.hlt.fnparse.datatypes.FNParse;
+import edu.jhu.hlt.fnparse.datatypes.Frame;
 import edu.jhu.hlt.fnparse.datatypes.FrameInstance;
+import edu.jhu.hlt.fnparse.datatypes.Sentence;
+import edu.jhu.hlt.fnparse.datatypes.Span;
 
 /**
  * Converts {@link FNParse}s to {@link FNParse}s. Specifically, takes FNParses
@@ -15,9 +22,9 @@ import edu.jhu.hlt.fnparse.datatypes.FrameInstance;
  * code.
  * 
  * The mapping used is:
- * k   -> 3 * k + 0
- * C-k -> 3 * k + 1
- * R-k -> 3 * k + 2
+ * k   -> k
+ * C-k -> k + K
+ * R-k -> k + 2*K
  *
  * So this will break the invariant that arguments.length == frame.numRoles(),
  * in fact arguments.length == 3 * frame.numRoles().
@@ -26,6 +33,41 @@ import edu.jhu.hlt.fnparse.datatypes.FrameInstance;
  */
 public class PropbankContRefRoleFNParseConverter {
 
-  // TODO
+  public static FNParse flatten(FNParse y) {
+    Sentence sent = y.getSentence();
+    List<FrameInstance> fis = new ArrayList<>();
+    for (FrameInstance fi : y.getFrameInstances()) {
+      int K = fi.getFrame().numRoles();
+
+      // Map the arguments over
+      Span[] args = new Span[K * 3];
+      Arrays.fill(args, Span.nullSpan);
+      for (int k = 0; k < K; k++) {
+        Span s = fi.getArgument(k);
+        args[k] = s;
+        List<Span> cont = fi.getContinuationRoleSpans(k);
+        List<Span> ref = fi.getReferenceRoleSpans(k);
+        if (cont.size() > 1) throw new RuntimeException();
+        if (ref.size() > 1) throw new RuntimeException();
+        if (cont.size() > 0)
+          args[k + K] = cont.get(0);
+        if (ref.size() > 0)
+          args[k + 2*K] = ref.get(0);
+      }
+
+      // Map the Frame over into a special dummy frame
+      Frame f = fi.getFrame();
+      String[] roles = new String[K * 3];
+      for (int k = 0; k < K; k++) {
+        roles[k] = f.getRole(k);
+        roles[k + K] = "C-" + f.getRole(k);
+        roles[k + 2*K] = "R-" + f.getRole(k);
+      }
+      Frame ff = new Frame(f.getId(), f.getName() + "-flat", null, roles);
+
+      fis.add(FrameInstance.newFrameInstance(ff, fi.getTarget(), args, sent));
+    }
+    return new FNParse(sent, fis);
+  }
 
 }
