ec94a554146e22f7132b2ffd24181dfd63290970
Travis Wolfe <twolfe18@gmail.com>
2016-01-19 14:16:26 -0500

    added new ShimModel.main qsub scripts

---
+++ scripts/precompute-features/train3-setup.sh
@@ -0,0 +1,69 @@
+
+# - convert pipeline over to something other than RerankerTrainer which is fubar
+#   - does the timing work? (for IO in particular)
+#     read all of propbank via PropbankReader: 72 seconds
+#     real all the features for one shard: 21 seoncds (261M, 24k lines, md5=fda87e019773379796f74461ea08dbe2)
+#     read bialph: 3 seconds (1.8G, 17M lines, md5=a7ba15f707a1351dee2ced7f8ee8bc48)
+#     compute feature products for one shard: 16 seconds (8-1280 feature set)
+# 
+#     (21 + 16) * 256 / 3600.0 = 2.6 hours
+#     => This is totally reasonable if you know its working, no threading needed
+# 
+#   - can I afford to do prediction on the dev and test set every epoch?
+#     => if not, just make a smaller version which you can so you know roughly how things are going -- should be easy to parse 300 sentences to get a good idea
+
+
+
+# Point of this script is to call ShimMode's main method
+# which produces a java serialized version of the data with all
+# the features computed and products computed.
+# This is the last step before running ShimModel in earnest to
+# get real results.
+
+DATASET=propbank
+PROPBANK=true
+DATA_HOME=/export/projects/twolfe/fnparse-data
+DD=/export/projects/twolfe/fnparse-output/experiments/dec-experiments/$DATASET
+CD=$DD/coherent-shards-filtered-small-jser
+mkdir -p $CD/sge-logs
+
+echo "copying features to a safe place..."
+mkdir -p $CD/features
+for f in scripts/having-a-laugh/propbank-*; do
+  echo "$f  ==>  $CD/features"
+  cp $f $CD/features
+done
+
+echo "copying jar to a safe place..."
+JAR=target/fnparse-1.0.6-SNAPSHOT-jar-with-dependencies.jar
+JAR_STABLE=$CD/fnparse.jar
+echo "    $JAR"
+echo "==> $JAR_STABLE"
+cp $JAR $JAR_STABLE
+
+for C in 4 8 16; do
+for D in 320 640 1280; do
+K="C$C-D$D"
+mkdir $CD/$K
+qsub -N "fc-$K" \
+  -b y -j y -V -o $CD/sge-logs \
+  -l 'mem_free=32G,num_proc=1,h_rt=12:00:00' \
+  java -Xmx30G -XX:+UseSerialGC -ea -server -cp $CD/fnparse.jar \
+    edu.jhu.hlt.fnparse.rl.rerank.ShimModel \
+    dontTrain true \
+    primesFile ${DATA_HOME}/primes/primes1.byLine.txt.gz \
+    data.framenet.root ${DATA_HOME} \
+    data.wordnet ${DATA_HOME}/wordnet/dict \
+    data.embeddings ${DATA_HOME}/embeddings \
+    data.ontonotes5 /export/common/data/corpora/LDC/LDC2013T19/data/files/data/english/annotations \
+    data.propbank.conll ${DATA_HOME}/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0/data \
+    data.propbank.frames ${DATA_HOME}/ontonotes-release-5.0-fixed-frames/frames \
+    bialph $DD/coherent-shards-filtered-small/alphabet.txt \
+    featuresParent $DD/coherent-shards-filtered-small/features \
+    featureSet $DD/features/$DATASET-$C-${D}.fs \
+    trainData $CD/$K/train.jser.gz \
+    devData $CD/$K/dev.jser.gz \
+    testData $CD/$K/test.jser.gz
+done
+done
+
---
+++ scripts/precompute-features/train3.sh
@@ -0,0 +1,76 @@
+#$ -j y
+#$ -V
+#$ -l h_rt=72:00:00
+#$ -l mem_free=31G
+#$ -l num_proc=1
+#$ -S /bin/bash
+
+set -eu
+
+echo "starting at `date` on $HOSTNAME in `pwd`"
+echo "args: $@"
+CPU=`grep -m 1 '^model name' /proc/cpuinfo | cut -d':' -f2 | xargs`
+echo "running on an $CPU"
+
+if [[ $# -lt 5 ]]; then
+  echo "1) a working directory (WD) for output"
+  echo "2) a data directory (DD) which contains (train|dev|test).jser.gz"
+  echo "3) dataset, i.e. either \"propbank\" or \"framenet\""
+  echo "4) what to set -Xmx to, e.g. \"22G\" -- you must set mem_free from above"
+  echo "5) a jar file in a stable location"
+  echo "6+) any other ExperimentProperties key-value config pairs"
+  exit 1
+fi
+
+WD=$1
+DD=$2
+DATASET=$3
+MEM=$4
+JAR=$5
+
+shift 5
+
+LOG=$WD/log.txt
+
+# TODO Make this smaller for framenet?
+NUM_MODEL_SHARDS=10
+
+# TODO Remove and test this. This should be automatically
+# derived inside CachedFeatures now.
+if [[ $DATASET == "propbank" ]]; then
+  NR=30
+  PROPBANK=true
+  MAX_EPOCH=2
+  NUM_INST=115000
+elif [[ $DATASET == "framenet" ]]; then
+  NR=60
+  PROPBANK=false
+  MAX_EPOCH=20
+  NUM_INST=3500
+else
+  echo "unknown dataset: $DATASET"
+  exit 2
+fi
+
+DATA_HOME=/export/projects/twolfe/fnparse-data
+
+java -Xmx30G -XX:+UseSerialGC -ea -server -cp $CD/fnparse.jar \
+  edu.jhu.hlt.fnparse.rl.rerank.ShimModel \
+  primesFile ${DATA_HOME}/primes/primes1.byLine.txt.gz \
+  data.framenet.root ${DATA_HOME} \
+  data.wordnet ${DATA_HOME}/wordnet/dict \
+  data.embeddings ${DATA_HOME}/embeddings \
+  data.ontonotes5 /export/common/data/corpora/LDC/LDC2013T19/data/files/data/english/annotations \
+  data.propbank.conll ${DATA_HOME}/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0/data \
+  data.propbank.frames ${DATA_HOME}/ontonotes-release-5.0-fixed-frames/frames \
+  bialph $DD/coherent-shards-filtered-small/alphabet.txt \
+  featuresParent $DD/coherent-shards-filtered-small/features \
+  featureSet $DD/features/$DATASET-$C-${D}.fs \
+  trainData $CD/$K/train.jser.gz \
+  devData $CD/$K/dev.jser.gz \
+  testData $CD/$K/test.jser.gz
+
+
+echo "ret code: $?"
+echo "done at `date`"
+
