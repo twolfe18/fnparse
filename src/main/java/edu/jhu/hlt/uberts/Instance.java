f30fee2cb6da169e4fb08988547686ba23458e1d
Travis Wolfe <twolfe18@gmail.com>
2016-03-02 13:20:01 -0500

    added Instance as building block for training code

---
+++ src/main/java/edu/jhu/hlt/uberts/Instance.java
@@ -0,0 +1,20 @@
+package edu.jhu.hlt.uberts;
+
+public interface Instance {
+
+  /**
+   * Add any presumed {@link HypEdge}s before starting inference.
+   */
+  void setupState(Uberts u);
+
+  /**
+   * +1 means a gold edge, good thing to add. -1 means a bad edge which
+   * introduces loss. You can play with anything in between.
+   *
+   * TODO This needs to be expanded upon. There are lots of small issues like
+   * you can just add this into the oracle, you have to know when something is
+   * perfectly correct vs not... etc.
+   */
+  double label(HypEdge e);
+
+}
