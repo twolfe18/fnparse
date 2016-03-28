7abbdf108c6826c82f7875c828c11e12bc3dc088
Travis Wolfe <twolfe18@gmail.com>
2016-03-28 11:21:01 -0400

    updated compute-ig.sh to use InformationGainProducts backend, outlined feature-selection.sh

--- scripts/precompute-features/compute-ig.sh
+++ scripts/precompute-features/compute-ig.sh
@@ -1,48 +1,73 @@
 #$ -j y
 #$ -V
 #$ -l h_rt=72:00:00
-#$ -l mem_free=20G
+#$ -l mem_free=7G
 #$ -l num_proc=1
 #$ -S /bin/bash
 
-set -e
+set -eu
 
 echo "starting at `date` on $HOSTNAME"
 echo "args: $@"
 
-if [[ $# != 7 ]]; then
-  echo "please provide:"
-  echo "1) a feature file parent directory"
-  echo "2) a feature file glob"
-  echo "3) an output file to put template IG estimates in"
-  echo "4) a jar file"
-  echo "5) a set of test set sentence ids to ignore"
-  echo "6) number of roles expected"
-  echo "7) how many GB of memory to give to the JVM (e.g. \"20\", NOT \"20G\")"
-  exit 1
-fi
+#if [[ $# != 7 ]]; then
+#  echo "please provide:"
+#  echo "1) a feature file parent directory"
+#  echo "2) a feature file glob"
+#  echo "3) an output file to put template IG estimates in"
+#  echo "4) a jar file"
+#  echo "5) a set of test set sentence ids to ignore"
+#  echo "6) number of roles expected"
+#  echo "7) how many GB of memory to give to the JVM (e.g. \"20\", NOT \"20G\")"
+#  exit 1
+#fi
 
 FEATS_PARENT=$1
 FEATS_GLOB=$2
-OUTPUT_IG_FILE=$3
-JAR=$4
-TEMPLATE_PROD_IG_OUTPUT=$5
-NUM_ROLES=$6
-XMX=$7
+BIALPH=$3             # names of features in FEATS_*
+OUTPUT_IG_FILE=$4
+ENTROPY_METHOD=$5     # BUB, MAP, or MLE
+LABEL_TYPE=$6         # frames or roles
+IS_PROPBANK=$7
+ROLE_NAMES=$8         # e.g. $WORKING_DIR/raw-shards/job-0-of-256/role-names.txt.bz2
+IGNORE_SENT_IDS=$9
+NUM_ROLES=${10}
+SHARD=${11}
+JAR=${12}
+XMX=${13}
+
+# Must be set in environment
+#FNPARSE_DATA=/export/projects/twolfe/fnparse-data/
 
 # Can't use relative paths when you have a checkout in a temporary directory
 # and don't use -cwd!
 #-DbubFuncParentDir=scripts/precompute-features
 
+# -Dunigrams=true is the flag to make InformationGainProducts
+# behave like InformationGain and only compute IG for single templates.
 java -Xmx${XMX}G -cp $JAR \
   -DfeaturesParent=$FEATS_PARENT \
   -DfeaturesGlob=$FEATS_GLOB \
+  -Dbialph=$BIALPH \
+  -Dshard=$SHARD \
   -DtopK=99999 \
   -DnumRoles=$NUM_ROLES \
   -DoutputFeatures=$OUTPUT_IG_FILE \
-  -DignoreSentenceIds=$TEMPLATE_PROD_IG_OUTPUT \
+  -Doutput=$OUTPUT_IG_FILE \
+  -DignoreSentenceIds=$IGNORE_SENT_IDS \
   -DbubFuncParentDir=/export/projects/twolfe/fnparse-data/matlab-code \
-  edu.jhu.hlt.fnparse.features.precompute.InformationGain
+  -Dpropbank=$IS_PROPBANK \
+  -DentropyMethod=$ENTROPY_METHOD \
+  -DlabelType=$LABEL_TYPE \
+  -Dunigrams=true \
+  -DroleNames=$ROLE_NAMES \
+  -Ddata.framenet.root=$FNPARSE_DATA \
+  -Ddata.wordnet=$FNPARSE_DATA/wordnet/dict \
+  -Ddata.embeddings=$FNPARSE_DATA/embeddings \
+  -Ddata.ontonotes5=$FNPARSE_DATA/LDC2013T19/data/files/data/english/annotations \
+  -Ddata.propbank.conll=$FNPARSE_DATA/conll-formatted-ontonotes-5.0/conll-formatted-ontonotes-5.0/data \
+  -Ddata.propbank.frames=$FNPARSE_DATA/ontonotes-release-5.0-fixed-frames/frames \
+  edu.jhu.hlt.fnparse.features.precompute.featureselection.InformationGainProducts
 
 echo "ret code: $?"
 echo "done at `date`"
--- scripts/precompute-features/feature-selection.sh
+++ scripts/precompute-features/feature-selection.sh
@@ -4,15 +4,38 @@
 set -eu
 
 WORKING_DIR=$1        # root of data dir, doesn't include e.g. $ENTROPY_METHOD
-TEST_SET_SENT_IDS=$2
-ENTROPY_METHOD=$3     # MAP, MLE, BUB
-JAR=$4
-SUF=$5
+ENTROPY_METHOD=$2     # MAP, MLE, BUB
+LABEL_TYPE=$3         # frames or roles
+IS_PROPBANK=$4
+JAR=$5
+SUF=$6
+
+TEST_SET_SENT_IDS=$WORKING_DIR/test-set-sentence-ids.txt
+if [[ ! -f $TEST_SET_SENT_IDS ]]; then
+  echo "couldn't find test sent ids: $TEST_SET_SENT_IDS"
+  exit 1
+fi
+
+# ig/
+#   bub/
+#     fnparse.jar
+#     products/
+#       ig-files/
+#       sge-logs/
+#     templates/
+#       full
+#       split10
+#       split10-filter10
+#       sge-logs/
+#     feature-sets/
+#       16-1280.fs
 
 # This for the output for a specific run of this script
 WD=$WORKING_DIR/ig/$ENTROPY_METHOD
 echo "putting output in $WD"
-sleep 10  # a chance to kill/abort
+#sleep 10  # a chance to kill/abort
+mkdir -p $WD
+
 
 JAR=target/fnparse-1.0.6-SNAPSHOT-jar-with-dependencies.jar
 JAR_STABLE=$WD/fnparse.jar
@@ -21,76 +44,80 @@ echo "    $JAR"
 echo "==> $JAR_STABLE"
 cp $JAR $JAR_STABLE
 
+# Used for capturing SGE output, for example
 TEMP=$WD/tempfile.txt
 
 
-# Do a 1/10 estimate of 1/10 of the data and average the answers (should take 1/10th the time but be approximate)
-N=10
-XMX=32
-XMX_SGE=36
-mkdir -p $WD/templates/split-$N-filter10
-for i in `seq $N | awk '{print $1-1}'`; do
-  qsub -N split$N-filter10-$i-ig-template \
-    -l "mem_free=${XMX_SGE}G" \
-    -o $WORKING_DIR/ig/templates/sge-logs \
-    scripts/precompute-features/compute-ig.sh \
-      $WORKING_DIR/coherent-shards-filtered/features \
-      "glob:**/*1${i}.txt$SUF" \
-      $WORKING_DIR/ig/templates/split-$N-filter10/shard-${i}.txt \
-      $JAR_STABLE \
+# Figure out how many roles there are.
+# This is needed for indexing (y/role, x/feature) pairs.
+# NOTE: index+1 goes from 0-index to count, and index+2 also accounts for
+# InformationGain.ADD_ONE=true which uses -1 as "noRole" => 0 as "noRole".
+ROLE_DICT="$WORKING_DIR/raw-shards/job-0-of-*/role-names.txt*"
+if [[ $LABEL_TYPE == "roles" ]]; then
+  NUM_ROLES=`compress-cat $ROLE_DICT | grep -oP '\d+(?=	r=.+)' | tail -n 1 | awk '{print $1+2}'`
+elif [[ $LABEL_TYPE == "frames" ]]; then
+  NUM_ROLES=`compress-cat $ROLE_DICT | grep -oP '\d+(?=	f=.+)' | tail -n 1 | awk '{print $1+2}'`
+else
+  echo "illegal LABEL_TYPE: $LABEL_TYPE"
+  exit 2
+fi
+echo "computed there are $NUM_ROLES from looking at $ROLE_DICT"
+
+
+### Step 1 ####################################################################
+echo "Computing IG for single templates @frame@role..."
+FEATS=$WORKING_DIR/coherent-shards/features
+BIALPH=$WORKING_DIR/coherent-shards/alphabet.txt$SUF
+mkdir -p $WD/templates/ig-files
+mkdir -p $WD/templates/sge-logs
+MEM=6
+NUM_SHARDS=500
+for I in `seq $NUM_SHARDS | awk '{print $i-1}'`; do
+  OUTPUT=$WD/templates/ig-files/shard-${I}-of-${NUM_SHARDS}.txt.gz
+  SHARD="$I/$NUM_SHARDS"
+  qsub -o $WD/templates/sge-logs \
+    ./scripts/precompute-features/compute-ig.sh \
+      $FEATS \
+      "glob:**/*" \
+      $BIALPH \
+      $OUTPUT \
+      $ENTROPY_METHOD \
+      $LABEL_TYPE \
+      $IS_PROPBANK \
+      $ROLE_DICT \
       $TEST_SET_SENT_IDS \
       $NUM_ROLES \
-      $XMX
+      $SHARD \
+      $JAR_STABLE \
+      $MEM
 done
 
 
-# Do a 1/10 estimate and average the answers (should take 1/10th the time but be approximate)
-N=10
-XMX=60
-XMX_SGE=65
-mkdir -p $WD/ig/templates/split-$N
-for i in `seq $N | awk '{print $1-1}'`; do
-  qsub -N split$N-$i-ig-template \
-    -l "mem_free=${XMX_SGE}G" \
-    -o $WORKING_DIR/ig/templates/sge-logs \
-    scripts/precompute-features/compute-ig.sh \
-      $WORKING_DIR/coherent-shards-filtered/features \
-      "glob:**/*${i}.txt$SUF" \
-      $WD/templates/split-$N/shard-${i}.txt \
-      $JAR_STABLE \
-      $TEST_SET_SENT_IDS \
-      $NUM_ROLES \
-      $XMX
-done >$TEMP
-
-# Do a full/proper estimate
-XMX=80
-XMX_SGE=85
-qsub -N full-ig-template \
-  -l "mem_free=${XMX_SGE}G" \
-  -o $WD/templates/sge-logs \
-  scripts/precompute-features/compute-ig.sh \
-    $WORKING_DIR/coherent-shards-filtered/features \
-    "glob:**/*" \
-    $WD/templates/full-ig.txt \
-    $JAR_STABLE \
-    $TEST_SET_SENT_IDS \
-    $NUM_ROLES \
-    $XMX
+### Step 2 ####################################################################
+echo "Reducing template@frame@role PMI scores to single template scores..."
+# TODO call combine-mutual-information.py
+# Crucial questions that I'd like answered:
+# - exp vs max
+# - score vs rank
+# => this gives me 3 at a minimum, but I can just do the fourth side of the square for completeness
+# exp+score (baseline)    1 0 0 1 0
+# exp+rank                1 0 0 0 1
+# max+score               0 1 0 1 0
+# max+rank                1 0 0 0 1
 
 
-### After those jobs finish, average things
-DEPS=`grep -oP '(?<=Your job )(\d+)' $TEMP | tr '\n' ',' | perl -pe 's/(.*),/\1\n/'`
-qsub -cwd -l "mem_free=1G,num_proc=1,h_rt=12:00:00" -b y -hold_jid $DEPS \
-  python scripts/precompute-features/average_ig2.py \
-    $WD/templates/split-$N/average.txt \
-    $WD/templates/split-$N/shard-* >$TEMP
-
-
-# 2) For products of templates, filtered by top K products ranked by product of template IG
+### Step 3 ####################################################################
+echo "Computing IG for product features heuristically chosen from template IG scores..."
 # NOTE: The more shards the more products we compute on
+
+# TODO This is probably broken!
 DEPS=`grep -oP '(?<=Your job )(\d+)' $TEMP | tr '\n' ',' | perl -pe 's/(.*),/\1\n/'`
+
+OUTPUT=$WD/templates/ig-files/shard-${I}-of-${NUM_SHARDS}.txt.gz
+
+# TODO Get this from output of step 2...
 TEMPLATE_IG_FILE=$WORKING_DIR/ig/templates/full-ig.txt
+
 PROD_IG_WD=$WORKING_DIR/ig/products
 mkdir -p $PROD_IG_WD/ig-files
 mkdir -p $PROD_IG_WD/sge-logs
@@ -113,22 +140,16 @@ for i in `seq $NUM_SHARDS | awk '{print $1 - 1}'`; do
 done
 
 
-# 3) TODO pull in code from scripts/having-a-laugh/Makefile
+### Step 4 ####################################################################
+echo "Reducing template@frame@role PMI scores to single feature scores..."
+# TODO
 
 
-# ig/
-#   bub/
-#     fnparse.jar
-#     products/
-#       ig-files/
-#       sge-logs/
-#     templates/
-#       full
-#       split10
-#       split10-filter10
-#       sge-logs/
-#     feature-sets/
-#       16-1280.fs
+### Step 5 ####################################################################
+echo "Computing feature sets based on IG scores and apparent redundancy (by feature name)..."
+# TODO see scripts/having-a-laugh
+
+
 
 
 
