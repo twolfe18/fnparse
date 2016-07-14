4e63bf2a83ad37d1b0eff87fda8866dba7270a93
Travis Wolfe <twolfe18@gmail.com>
2016-07-13 18:51:13 -0700

    added arg4target and frequency AgendaPriority functions

--- src/main/java/edu/jhu/hlt/uberts/AgendaPriority.java
+++ src/main/java/edu/jhu/hlt/uberts/AgendaPriority.java
@@ -1,12 +1,21 @@
 package edu.jhu.hlt.uberts;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 
+import edu.jhu.hlt.tutils.Counts;
+import edu.jhu.hlt.tutils.ExperimentProperties;
 import edu.jhu.hlt.tutils.Log;
 import edu.jhu.hlt.tutils.Span;
+import edu.jhu.hlt.tutils.TimeMarker;
 import edu.jhu.hlt.tutils.scoring.Adjoints;
+import edu.jhu.hlt.uberts.io.ManyDocRelationFileIterator;
+import edu.jhu.hlt.uberts.io.ManyDocRelationFileIterator.RelDoc;
+import edu.jhu.hlt.uberts.io.RelationFileIterator;
+import edu.jhu.hlt.uberts.io.RelationFileIterator.RelLine;
 import edu.jhu.prim.tuple.Pair;
 
 public interface AgendaPriority extends BiFunction<HypEdge, Adjoints, Double> {
@@ -56,6 +65,12 @@ public interface AgendaPriority extends BiFunction<HypEdge, Adjoints, Double> {
       return new Dfs();
     case "bfs":
       return new Bfs();
+    case "arg4target":
+      return new Arg4Target();
+    case "frequency":
+      ExperimentProperties config = ExperimentProperties.getInstance();
+      List<File> train = config.getExistingFiles("train.facts");
+      return new Arg4ByRoleFrequency(train.get(0));
     default:
       throw new IllegalArgumentException("don't know about: " + name);
     }
@@ -117,22 +132,74 @@ public interface AgendaPriority extends BiFunction<HypEdge, Adjoints, Double> {
   }
 
   public static class Bfs implements AgendaPriority {
+    private Dfs dfs = new Dfs();
     @Override
     public double priority(HypEdge edge, Adjoints score) {
-      switch (edge.getRelation().getName()) {
-      case "event1":
-        return +2;
-      case "predicate2":
-        return +1;
-      case "srl2":
+      return -dfs.priority(edge, score);
+    }
+  }
+
+  public static class Arg4Target implements AgendaPriority {
+    @Override
+    public double priority(HypEdge edge, Adjoints score) {
+      if (!edge.getRelation().getName().equals("argument4"))
         return 0;
-      case "srl3":
-        return -1;
-      case "argument4":
-        return -2;
-      default:
-        throw new RuntimeException("don't know about: " + edge);
+      Span t = Span.inverseShortString((String) edge.getTail(0).getValue());
+      return -t.start;
+    }
+  }
+
+  public static class Arg4ByRoleFrequency implements AgendaPriority {
+    private Counts<String> roleCounts;
+    private File providence;
+    public Arg4ByRoleFrequency(File containsArgument4Facts) {
+      Log.info("reading role counts from " + containsArgument4Facts.getPath());
+      roleCounts = new Counts<>();
+      providence = containsArgument4Facts;
+      TimeMarker tm = new TimeMarker();
+      try (RelationFileIterator rels = new RelationFileIterator(containsArgument4Facts, false);
+          ManyDocRelationFileIterator many = new ManyDocRelationFileIterator(rels, true)) {
+        int docs = 0;
+        while (many.hasNext()) {
+          docs++;
+          RelDoc d = many.next();
+          assert d.facts.isEmpty();
+          for (RelLine l : d.items) {
+            if (l.tokens[1].equals("argument4")) {
+              String k = l.tokens[2+3];
+              roleCounts.increment(k);
+            }
+          }
+
+          if (tm.enoughTimePassed(15)) {
+            Log.info("totalCount=" + roleCounts.getTotalCount()
+                + " docs=" + docs
+                + " after " + tm.secondsSinceFirstMark() + " seconds");
+          }
+        }
+      } catch (IOException e) {
+        throw new RuntimeException(e);
       }
+      Log.info("done, counts: " + roleCounts);
+    }
+    public File getProvidence() {
+      return providence;
+    }
+    @Override
+    public double priority(HypEdge edge, Adjoints score) {
+      String r = edge.getRelation().getName();
+      if (!r.equals("argument4")) {
+        // Not relevant
+        return 0;
+      }
+      String k = (String) edge.getTail(3).getValue();
+      int c = roleCounts.getCount(k);
+      if (c == 0)
+        Log.info("WARNING: unseen role: " + k);
+      double p = ((double) c) / roleCounts.getTotalCount();
+//      if (Agenda.DEBUG)
+//        Log.info("p=" + p + " " + edge);
+      return p;
     }
   }
 
