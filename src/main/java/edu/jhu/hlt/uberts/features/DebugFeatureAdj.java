75fe8020e53190939fb86896fe73945f88477c70
Travis Wolfe <twolfe18@gmail.com>
2016-07-21 22:27:41 -0700

    added Adjoints for debugging, shows fy and fx by name/string

---
+++ src/main/java/edu/jhu/hlt/uberts/features/DebugFeatureAdj.java
@@ -0,0 +1,51 @@
+package edu.jhu.hlt.uberts.features;
+
+import java.util.List;
+
+import edu.jhu.hlt.tutils.scoring.Adjoints;
+
+/*
+ * The problem is that I use AveragedPerceptronWeights.adj
+ */
+public class DebugFeatureAdj implements Adjoints {
+
+  private boolean showWrapped = false;
+  private Adjoints wrapped;
+
+  // These are for debugging
+  private List<String> fy;
+  private List<String> fx;
+  private Object ancillaryInfo;
+
+  public DebugFeatureAdj(Adjoints wrapped, List<String> fy, List<String> fx, Object ancillaryInfo) {
+    this.wrapped = wrapped;
+    this.fy = fy;
+    this.fx = fx;
+    this.ancillaryInfo = ancillaryInfo;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("(DFA");
+    if (ancillaryInfo != null)
+      sb.append(" " + ancillaryInfo);
+    sb.append(" fy=" + fy);
+    sb.append(" fx=" + fx);
+    if (showWrapped) {
+      sb.append(" " + wrapped);
+    }
+    sb.append(')');
+    return sb.toString();
+  }
+
+  @Override
+  public double forwards() {
+    return wrapped.forwards();
+  }
+
+  @Override
+  public void backwards(double dErr_dForwards) {
+    System.out.printf("[DebugFeatureAdj backwards] dErr=%+.1f %s\n", dErr_dForwards, this);
+    wrapped.backwards(dErr_dForwards);
+  }
+}
