d2dba8a1984cac213de54e52405d627e21cca766
Travis Wolfe <twolfe18@gmail.com>
2017-01-25 11:02:43 -0500

    put StringTermVec in its own file

---
+++ src/main/java/edu/jhu/hlt/ikbp/tac/StringTermVec.java
@@ -0,0 +1,52 @@
+package edu.jhu.hlt.ikbp.tac;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import edu.jhu.hlt.concrete.Communication;
+
+/**
+ * Holds tf not idf.
+ */
+public class StringTermVec implements Iterable<Entry<String, Double>>, Serializable {
+  private static final long serialVersionUID = -284690402445952436L;
+
+  private Map<String, Double> tf;
+  private double z;
+  
+  public StringTermVec() {
+    tf = new HashMap<>();
+    z = 0;
+  }
+  
+  public StringTermVec(Communication c) {
+    this();
+    if (c == null)
+      throw new IllegalArgumentException();
+    for (String s : IndexCommunications.terms(c))
+      add(s, 1);
+  }
+  
+  public double getTotalCount() {
+    return z;
+  }
+
+  public void add(StringTermVec other) {
+    for (Entry<String, Double> e : other.tf.entrySet())
+      add(e.getKey(), e.getValue());
+  }
+  
+  public void add(String word, double count) {
+    double prev = tf.getOrDefault(word, 0d);
+    tf.put(word, prev + count);
+    z += count;
+  }
+
+  @Override
+  public Iterator<Entry<String, Double>> iterator() {
+    return tf.entrySet().iterator();
+  }
+}
