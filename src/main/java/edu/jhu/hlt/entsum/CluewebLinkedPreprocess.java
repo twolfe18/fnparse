fe0f3f4769e17bd97463b22c6e83e604fa0c9d21
Travis Wolfe <twolfe18@gmail.com>
2017-03-03 13:03:51 -0500

    removed junk

--- src/main/java/edu/jhu/hlt/entsum/CluewebLinkedPreprocess.java
+++ src/main/java/edu/jhu/hlt/entsum/CluewebLinkedPreprocess.java
@@ -11,9 +11,11 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
+import edu.jhu.hlt.concrete.simpleaccumulo.TimeMarker;
 import edu.jhu.hlt.entsum.CluewebLinkedPreprocess.EntitySplit.DataSetSplit;
 import edu.jhu.hlt.entsum.CluewebLinkedSentence.Link;
 import edu.jhu.hlt.entsum.CluewebLinkedSentence.ValidatorIterator;
+import edu.jhu.hlt.fnparse.util.Describe;
 import edu.jhu.hlt.tutils.Counts;
 import edu.jhu.hlt.tutils.ExperimentProperties;
 import edu.jhu.hlt.tutils.FileUtil;
@@ -22,13 +24,14 @@ import edu.jhu.hlt.tutils.rand.ReservoirSample;
 import edu.jhu.prim.tuple.Pair;
 import edu.jhu.util.CountMinSketch.StringCountMinSketch;
 
+/**
+ * 1) extract counts for all entities, use this to produce a train/dev/test set
+ * 2) TODO for each, extract all those sentences, put them into a separate file/DB
+ * 3) TODO for each entity file, parse and tag them with PMP
+ *
+ * @author travis
+ */
 public class CluewebLinkedPreprocess {
-  
-  static class MID {
-    byte[] bs;
-    int hc;
-    
-  }
   
   /**
    * First pass: get an approximate frequency for all the entities
@@ -66,13 +69,16 @@ public class CluewebLinkedPreprocess {
     }
   }
   
+  /**
+   * Logic for actually splitting entities into train/dev/test partitions by frequency.
+   */
   static class EntitySplit {
     static final Charset UTF8 = Charset.forName("UTF-8");
     
     // Ahead of time measure how frequent each mid is (approximately)
     private EntCounts entCounts;
     private Random rand;
-    private Set<String> seenMids = new HashSet<>();
+    private Set<String> seenMids = new HashSet<>();   // TODO O(n) memory...
     
     // These two are indexed together
     private double[] thresholds;
@@ -145,7 +151,6 @@ public class CluewebLinkedPreprocess {
       
       public DataSetSplit(int nDev, int nTest, List<String> all, double cMin, double cMax) {
         if (nDev + nTest >= all.size()) {
-//          throw new IllegalArgumentException("nDev=" + nDev + " nTest=" + nTest + " all=" +all.size());
           Log.info("WARNING: there aren't enough instances: nDev=" + nDev + " nTest=" + nTest + " all=" +all.size());
         }
         this.cMin = cMin;
@@ -170,18 +175,18 @@ public class CluewebLinkedPreprocess {
         s.add(new Pair<>("nTrain", ""+train.size()));
         s.add(new Pair<>("nDev", ""+dev.size()));
         s.add(new Pair<>("nTest", ""+test.size()));
-        s.add(new Pair<>("nTrainUniq", ""+nUniq(train)));
-        s.add(new Pair<>("nDevUniq", ""+nUniq(dev)));
-        s.add(new Pair<>("nTestUniq", ""+nUniq(test)));
+//        s.add(new Pair<>("nTrainUniq", ""+nUniq(train)));
+//        s.add(new Pair<>("nDevUniq", ""+nUniq(dev)));
+//        s.add(new Pair<>("nTestUniq", ""+nUniq(test)));
         s.add(new Pair<>("types", ""+types));
         return s;
       }
       
-      public static <T> int nUniq(List<T> items) {
-        Set<T> u = new HashSet<>();
-        u.addAll(items);
-        return u.size();
-      }
+//      public static <T> int nUniq(List<T> items) {
+//        Set<T> u = new HashSet<>();
+//        u.addAll(items);
+//        return u.size();
+//      }
     }
     
     public DataSetSplit[] draw() {
@@ -202,13 +207,12 @@ public class CluewebLinkedPreprocess {
   }
   
   public static void buildTrainDevTestSplit(ExperimentProperties config) throws IOException {
+    TimeMarker tm = new TimeMarker();
 
     File linkedCluewebRoot = config.getExistingDir("linkedCluewebRoot",
         new File("/home/travis/code/data/clueweb09-freebase-annotation/extractedAnnotation"));
     List<File> fs = FileUtil.find(linkedCluewebRoot, "glob:**/*.gz");
     Log.info("found " + fs.size() + " *.gz files in linkedCluewebRoot=" + linkedCluewebRoot.getPath());
-    
-    fs = ReservoirSample.sample(fs, 200, new Random(9001));
     
     File outputDir = config.getOrMakeDir("outputDir");
     Log.info("outputDir=" + outputDir.getPath());
@@ -221,6 +225,8 @@ public class CluewebLinkedPreprocess {
         while (iter.hasNext()) {
           CluewebLinkedSentence sent = iter.next();
           ec.observe(sent);
+          if (tm.enoughTimePassed(10))
+            Log.info(Describe.memoryUsage());
         }
       }
     }
@@ -240,6 +246,8 @@ public class CluewebLinkedPreprocess {
         while (iter.hasNext()) {
           CluewebLinkedSentence sent = iter.next();
           es.add(sent);
+          if (tm.enoughTimePassed(10))
+            Log.info(Describe.memoryUsage());
         }
       }
     }
@@ -260,12 +268,6 @@ public class CluewebLinkedPreprocess {
         }
       }
     }
-//    try (BufferedWriter w = FileUtil.getWriter(new File(outputDir, "bucket-mid-frequencies.txt"))) {
-//      for (int i = 0; i < tdt.length; i++) {
-//        w.write("rare" + i + "\t" + tdt[i].cMin + "\t" + tdt[i].cMax);
-//        w.newLine();
-//      }
-//    }
     
     Log.info("done");
   }
@@ -277,8 +279,5 @@ public class CluewebLinkedPreprocess {
 
   /*
    * TODO
-   * 1) extract counts for all entities, use this to produce a train/dev/test set
-   * 2) for each, extract all those sentences, put them into a separate file/DB
-   * 3) for each entity file, parse and tag them with PMP
    */
 }
