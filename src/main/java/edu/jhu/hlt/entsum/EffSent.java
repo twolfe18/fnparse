c36e6247f9360573ab41b7680eb11dd5b69db9cd
Travis Wolfe <twolfe18@gmail.com>
2017-03-27 11:59:11 -0400

    bugfixes and better sentence dedup for distsup

--- src/main/java/edu/jhu/hlt/entsum/EffSent.java
+++ src/main/java/edu/jhu/hlt/entsum/EffSent.java
@@ -7,10 +7,16 @@ import java.io.Serializable;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import edu.jhu.hlt.entsum.CluewebLinkedPreprocess.PrepareSentencesForParsey;
+import edu.jhu.hlt.ikbp.tac.ComputeIdf;
 import edu.jhu.hlt.tutils.ExperimentProperties;
 import edu.jhu.hlt.tutils.FileUtil;
 import edu.jhu.hlt.tutils.Log;
@@ -49,6 +55,14 @@ public class EffSent implements Serializable {
     
     public String getFullMid() {
       return "/m/" + new String(mid, UTF8);
+    }
+    public String getAbbrMid() {
+      return new String(mid, UTF8);
+    }
+    
+    @Override
+    public String toString() {
+      return "(" + getFullMid() + " " + span().shortString() + " h=" + head + ")";
     }
   }
   
@@ -106,16 +120,168 @@ public class EffSent implements Serializable {
     return entities;
   }
   
+  public void showConllStyle(MultiAlphabet a) {
+    String[] mids = new String[parse.length];
+    Arrays.fill(mids, "_");
+    for (int i = 0; i < entities.length; i++) {
+      for (int j = entities[i].start; j < entities[i].end; j++) {
+        mids[j] = entities[i].getFullMid();
+        if (j == entities[i].head)
+          mids[j] += " *head";
+      }
+    }
+    
+    for (int i = 0; i < parse.length; i++) {
+      System.out.printf("% 4d  %-16s %s\n", i, a.word(parse[i].word), mids[i]);
+    }
+  }
+
+  /**
+   * The reason I want this is that there appears to be duplicates still getting through, e.g.
+   *   tokenized-sentences/dev/m.017kjq/sentences.txt
+   *   2000 ([FREEBASE mid=/m/0hsqf Seoul]Seoul[/FREEBASE]-[FREEBASE mid=/m/017kjq Yonhap News Agency]Yonhap News Agency[/FREEBASE]
+   *   2000 ([FREEBASE mid=/m/0hsqf Seoul]Seoul[/FREEBASE]-[FREEBASE mid=/m/017kjq Yonhap News Agency]Yonhap News Agency[/FREEBASE]).
+   *
+   * I think the key where I just have ordered-mids is not good enough, especially when there are only 2 mids for example.
+   * I think a better key to dedup on is:
+   *   <orderedMids> ++ <threeHighestIdfWordsInSentenceOrder>
+   */
+  public static class DedupMaW3Iter implements Iterator<EffSent>, AutoCloseable {
+    private Iter wrapped;
+    private ComputeIdf df;
+    private int numWords;
+    private Set<String> seen;
+    private EffSent cur;
+    private int total, skipped;
+    
+    public DedupMaW3Iter(Iter wrapped, ComputeIdf df, int numWordsInKey) {
+      this.wrapped = wrapped;
+      this.df = df;
+      this.numWords = numWordsInKey;
+      this.seen = new HashSet<>();
+      total = skipped = 0;
+      advance();
+    }
+    
+    static class WM {
+      int tokenIdx;
+      double idf;
+      public WM(int tok, double idf) {
+        this.tokenIdx = tok;
+        this.idf = idf;
+      }
+      public static final Comparator<WM> BY_IDF_DESC = new Comparator<WM>() {
+        @Override
+        public int compare(WM o1, WM o2) {
+          if (o1.idf > o2.idf)
+            return -1;
+          if (o2.idf > o1.idf)
+            return +1;
+          return 0;
+        }
+      };
+      public static final Comparator<WM> BY_TOKEN_ASC = new Comparator<WM>() {
+        @Override
+        public int compare(WM o1, WM o2) {
+          if (o1.tokenIdx < o2.tokenIdx)
+            return -1;
+          if (o2.tokenIdx < o1.tokenIdx)
+            return +1;
+          return 0;
+        }
+      };
+    }
+    
+    public String computeKey(EffSent s) {
+      
+      // Find the ordered set of mids
+      BitSet midTokens = new BitSet();
+      StringBuilder key = new StringBuilder();
+      int n = cur.numMentions();
+      for (int i = 0; i < n; i++) {
+        Mention m = cur.mention(i);
+        String mid = m.getAbbrMid();
+        if (i > 0)
+          key.append('_');
+        key.append(mid);
+        
+        for (int t = m.start; t < m.end; t++)
+          midTokens.set(t);
+      }
+
+      // Find the k-highest-idf words (which aren't inside an mention)
+      List<WM> words = new ArrayList<>();
+      MultiAlphabet a = wrapped.getParseAlph();
+      for (int i = 0; i < cur.parse.length; i++) {
+        if (midTokens.get(i))
+          continue;
+        String w = a.word(cur.parse[i].word);
+        double idf = df.idf(w);
+        words.add(new WM(i, idf));
+      }
+      Collections.sort(words, WM.BY_IDF_DESC);
+      while (words.size() > numWords)
+        words.remove(words.size()-1);
+      Collections.sort(words, WM.BY_TOKEN_ASC);
+      for (WM t : words) {
+        String w = a.word(cur.parse[t.tokenIdx].word);
+        key.append('#');
+        key.append(w);
+      }
+      
+      return key.toString();
+    }
+    
+    private void advance() {
+      boolean disable = false;
+      cur = null;
+      while (wrapped.hasNext()) {
+        total++;
+        if (total % 1000 == 0)
+          Log.info("skipped=" + skipped + " total=" + total + " uniq=" + seen.size());
+        cur = wrapped.next();
+        if (disable || seen.add(computeKey(cur)))
+          break;
+        cur = null;
+        skipped++;
+      }
+    }
+
+    @Override
+    public void close() throws IOException {
+      Log.info("skipped=" + skipped + " total=" + total + " uniq=" + seen.size());
+      wrapped.close();
+    }
+
+    @Override
+    public boolean hasNext() {
+      return cur != null;
+    }
+
+    @Override
+    public EffSent next() {
+      EffSent c = cur;
+      advance();
+      return c;
+    }
+  }
+  
   public static class Iter implements Iterator<EffSent>, AutoCloseable {
     private DepNode.ConllxFileReader iter;
     private BufferedReader r;
     private EffSent cur;
+    private MultiAlphabet parseAlph;
     
     public Iter(File parses, File mentions, MultiAlphabet a) throws IOException {
       Log.info("parses=" + parses.getPath() + " mentions=" + mentions.getPath());
       iter = new DepNode.ConllxFileReader(parses, a);
       r = FileUtil.getReader(mentions);
+      this.parseAlph = a;
       advance();
+    }
+    
+    public MultiAlphabet getParseAlph() {
+      return parseAlph;
     }
 
     private void advance() throws IOException {
