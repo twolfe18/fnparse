e0b3ef7bb08fa4f05f2e5303e2f7f06e093c4459
Travis Wolfe <twolfe18@gmail.com>
2017-04-05 15:55:09 -0400

    MultiMap can now sample and remove keys

--- src/main/java/edu/jhu/util/MultiMap.java
+++ src/main/java/edu/jhu/util/MultiMap.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Random;
 
 /**
  * A map which associates a list of values with a given key.
@@ -19,6 +21,19 @@ public class MultiMap<K, V> {
   
   public MultiMap() {
     map = new HashMap<>();
+  }
+  
+  public K sampleKeyBasedOnNumEntries(Random rand) {
+    if (map.isEmpty())
+      throw new IllegalStateException("map is empty, can't sample");
+    int thresh = rand.nextInt(numEntries);
+    int sum = 0;
+    for (Entry<K, List<V>> e : map.entrySet()) {
+      sum += e.getValue().size();
+      if (sum >= thresh)
+        return e.getKey();
+    }
+    throw new RuntimeException("problem: sum=" + sum + " numEntries=" + numEntries + " thresh=" + thresh);
   }
   
   public void sortValues(Comparator<V> c) {
@@ -39,6 +54,13 @@ public class MultiMap<K, V> {
     if (v == null)
       v = Collections.emptyList();
     return v;
+  }
+  
+  public List<V> remove(K key) {
+    List<V> r = map.remove(key);
+    if (r != null)
+      numEntries -= r.size();
+    return r;
   }
   
   public boolean add(K key, V value) {
